cbbbfa3fdadd36187bc5a38b80d30cdc5cd592be
kalalruturaj <ruturajkalal@gmail.com>
2016-03-28 20:15:43 -0400

    Initial commit

---
+++ app/src/main/java/com/example/ruturaj/techtalk_term_project/RegisterActivity.java
@@ -0,0 +1,15 @@
+package com.example.ruturaj.techtalk_term_project;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Ruturaj on 28/03/2016.
+ */
+public class RegisterActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register_activity);
+    }
+}
